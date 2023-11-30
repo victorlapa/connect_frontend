afc9131771646c8425cec5ca1d0605806acceef9
Victor Lapa <victtorlapa1@gmail.com>
2023-11-30 19:12:15 -0300

    palette

---
+++ lib/const/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color.fromARGB(255, 167, 33, 65);
+  static const Color accentColor = Colors.white;
+  static const Color buttonBorderColor = Colors.white;
+  static const Color buttonText = Colors.white;
+}
